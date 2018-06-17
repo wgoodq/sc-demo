6173d2db9bcb9cdeebf72d34496e49b3660031db
kyou <wgoodq@foxmail.com>
2018-06-17 22:59:55 +0800

    eureka-config-server -> eureka-server

---
+++ eureka-server/run_eureka_servers.sh
@@ -0,0 +1,24 @@
+# 查询原有进程
+echo 'eureka-server threads: '
+ps -ef | grep eureka-server | grep -Ev 'grep | run_eureka_servers.sh'
+
+# 终止原有进程
+ps -ef | grep eureka-server | grep -Ev 'grep | run_eureka_servers.sh' | awk '{print $2}' | xargs kill -9
+echo 'killing them...'
+sleep 1
+
+# 检查终止结果
+echo 'eureka-server threads: '
+ps -ef | grep eureka-server | grep -Ev 'grep | run_eureka_servers.sh'
+echo ''
+
+# 分割线
+echo '------------Closed------------All------------Servers------------'
+
+# 编译工程
+gradle clean build
+
+# 启动注册中心集群
+nohup java -jar ./build/libs/eureka-server-0.0.1.jar --spring.profiles.active=peer1 > ./build/libs/eureka_server_1.log 2>&1 &
+nohup java -jar ./build/libs/eureka-server-0.0.1.jar --spring.profiles.active=peer2 > ./build/libs/eureka_server_2.log 2>&1 &
+tail -f ./build/libs/eureka_server_1.log
--- eureka-server/src/main/java/cn/ok/es/EurekaServerApplication.java
+++ eureka-server/src/main/java/cn/ok/es/EurekaServerApplication.java
@@ -1,17 +1,19 @@
-package cn.ok.ecs;
+package cn.ok.es;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
+ * EnableEurekaServer: 启用注册中心
+ *
  * @author kyou on 2018-06-17 09:27:32
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaConfigServerApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaConfigServerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
